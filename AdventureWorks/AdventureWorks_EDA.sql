97487fd15af23ddcc0f4a1ddd399f9e7503bdb6d
James Laurie <103063112+Jimmy90s@users.noreply.github.com>
2022-06-25 16:58:00 -0700

    Add files via upload

--- AdventureWorks/AdventureWorks_EDA.sql
+++ AdventureWorks/AdventureWorks_EDA.sql
@@ -1,5 +1,5 @@
 /*
-
+https://sqlzoo.net/wiki/AdventureWorks
 */
 
 -- Concat first and last name
@@ -56,7 +56,72 @@ ON a.SalesOrderID = b.SalesOrderID
 
 -- A "Single Item Order" is a customer order where only one item is ordered.
 -- Show the SalesOrderID and the UnitPrice for every Single Item Order.
+SELECT SalesOrderID ,UnitPrice
+FROM AdventureWorksLT2019.SalesLT.SalesOrderDetail 
+WHERE OrderQty = 1
+
+-- Where did the racing socks go? List the product name and the CompanyName for all Customers who ordered ProductModel 'Racing Socks'.
+WITH CTE 
+AS(
+    SELECT SalesOrderID, Name, OrderQty
+    FROM AdventureWorksLT2019.SalesLT.Product a
+    JOIN AdventureWorksLT2019.SalesLT.SalesOrderDetail b
+    ON a.ProductID = b.ProductID
+    WHERE a.Name LIKE '%Racing Socks%'
+),
+CTE2
+AS(
+    SELECT a.SalesOrderID, CompanyName
+    FROM AdventureWorksLT2019.SalesLT.SalesOrderHeader a
+    JOIN AdventureWorksLT2019.SalesLT.Customer b
+    ON a.CustomerID = b.CustomerID
+)
+SELECT Name , CompanyName
+FROM CTE a
+JOIN CTE2 b
+ON a.SalesOrderID = b.SalesOrderID
 
 
+-- Use the SubTotal value in SaleOrderHeader to list orders from the largest to the smallest. 
+-- For each order show the CompanyName and the SubTotal and the total weight of the order.
+SELECT SubTotal
+FROM AdventureWorksLT2019.SalesLT.SalesOrderHeader 
+ORDER BY 1 DESC
 
+-- How many products in ProductCategory 'Cranksets' have been sold to an address in 'London'?
+
+WITH CTE 
+AS(
+    SELECT a.Name, b.ProductID
+    FROM AdventureWorksLT2019.SalesLT.ProductCategory a 
+    JOIN AdventureWorksLT2019.SalesLT.Product b 
+    ON a.ProductCategoryID = b.ProductCategoryID
+    WHERE a.Name LIKE '%Cranksets'
+),
+CTE2
+AS(
+    SELECT CustomerID ,City
+    FROM AdventureWorksLT2019.SalesLT.Address a 
+    JOIN AdventureWorksLT2019.SalesLT.CustomerAddress b 
+    ON a.AddressID = b.AddressID
+    WHERE City = 'London'  
+),
+CTE3
+AS(
+    SELECT a.CustomerID , b.City, a.SalesOrderID
+    FROM AdventureWorksLT2019.SalesLT.SalesOrderHeader a 
+    JOIN CTE2 b 
+    ON a.CustomerID = b.CustomerID
+),
+CTE4
+AS(
+    SELECT ProductID
+    FROM AdventureWorksLT2019.SalesLT.SalesOrderDetail a 
+    JOIN CTE3 b
+    ON a.SalesOrderID = b.SalesOrderID
+)
+SELECT COUNT(*)
+FROM CTE a 
+JOIN CTE4 b 
+ON a.ProductID = b.ProductID
 
